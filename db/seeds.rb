f3509fc16a8a4234b7df38e2ad8fd26be00add79
Kip Kosek <kipkosek@gmail.com>
2017-01-19 16:02:35 -0500

    US6 Seeding Data: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Create Standard Users
+5.times do
+  User.create!(
+  username: Faker::StarWars.unique.character,
+  email: Faker::Internet.unique.email,
+  password: Faker::Internet.unique.password(8)
+  )
+end
+users = User.all
+
+#Create Premium User
+premium = User.create!(
+  username: 'Premium User',
+  email: 'premium@example.com',
+  password: 'helloworld',
+  role: 'premium'
+)
+
+#Create Admin User
+admin = User.create!(
+  username: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+)
+
+#Create Public Wikis
+20.times do
+  wiki = Wiki.create!(
+    user: users.sample,
+    title: Faker::StarWars.unique.planet,
+    body: Faker::StarWars.unique.quote
+  )
+end
+wikis = Wiki.all
+
+#Create Private Wiki
+wiki_private = Wiki.create!(
+  user: premium,
+  title: "Private Test Wiki",
+  body: "Text of private test wiki",
+  private: true
+)
+
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
