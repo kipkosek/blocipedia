d425854b6afb4de9d73fe931c7648053f618bc54
Kip Kosek <kipkosek@gmail.com>
2017-01-25 15:40:42 -0500

    US7 Upgrading Account: Completed

--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
-include 'random_data'
 
 RSpec.describe Wiki, type: :model do
-let(:user) { create(:user) }
-let(:wiki) { create(:wiki) }
+  let(:user) { create(:user) }
+  let(:wiki) { create(:wiki) }
 
-it { is_expected.to validate_presence_of(:title) }
-it { is_expected.to validate_presence_of(:body) }
-it { is_expected.to validate_presence_of(:user) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:user) }
 
-describe "attributes" do
-  it "has title, body, and user attributes" do
-    expect(wiki).to have_attributes(title: wiki.title, body: wiki.body, user: wiki.user)
+  describe "attributes" do
+    it "has title, body, and user attributes" do
+      expect(wiki).to have_attributes(title: wiki.title, body: wiki.body, user: wiki.user)
+    end
   end
-end
 
 end
